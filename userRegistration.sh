ebc48b615ce78ac6c478b6fc98c4826bad6cd084
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-15 12:00:50 +0530

    Validation of Mobile Number

--- userRegistration.sh
+++ userRegistration.sh
@@ -45,10 +45,24 @@ function validateEmail()
 	fi
 }
 
+function validateMobileNumber()
+{
+	echo "Enter your Mobile Number"
+	read number
+	numberpattern="^[0-9]{1,}[[:space:]][0-9]{10}$"
+	if [[ $number =~ $numberpattern ]]
+	then
+		echo "Mobile Number is valid"
+	else
+		echo "Mobile number is invalid"
+	fi
+}
+
 main()
 {
 validateFirstName
 validateLastName
 validateEmail
+validateMobileNumber
 }
 main
