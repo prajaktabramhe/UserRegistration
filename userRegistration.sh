eb8d23705028a83940f7b815a1459f198e931241
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-15 11:42:11 +0530

    Validation of First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,24 @@
-#!/bin/bash 
+#!/bin/bash
 
-echo "Welcome to user registration"
+echo "Welcome in user registration";
+
+function validateFirstName()
+{
+       	echo "Enter Your first name";
+	read firstname
+
+	pattern="^([A-Z]{1})([a-z]{2})$";
+
+	if [[ $firstname =~ $pattern ]]
+       	  then
+		echo "Name is valid";
+	  else
+		echo "Name is invalid";
+	fi
+}
+
+function main()
+{
+	validateFirstName
+}
+main
